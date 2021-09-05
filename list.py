b97e158c0ca666a3637deb73a707917987589c97
Shane Pham <sambidao88@gmail.com>
2021-09-05 17:48:35 +0000

    list updated

--- list.py
+++ list.py
@@ -1,3 +1,17 @@
 x = 0.3
+
+# Data structure
 # List can store any type data, including Boolean
-li = [0, 2, "4ever", True, x ] 
+
+li = [0, 2, "4ever", True, x ]
+
+# Format a list like portrail
+lzd_cart = [
+  'pillow',
+  'book',
+  'smartphone',
+  'instant noodle'
+]
+
+# Can slice a list like this
+print(lzd_cart[0::3])
